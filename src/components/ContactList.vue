5b8be02e2987d3873cf5c10908a73faae4441f2b
Ngoc Khuu <khuuthibichngoc999@gmail.com>
2024-03-28 08:25:59 +0700

    Tao trang cap nhat lien he

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,21 +1,22 @@
 <script>
-    export default {
-        props: {
-            contacts: { type: Array, default: []},
-            activeIndex: { type: Number, default: -1}
-        },
-        emits: ["update:activeIndex"],
-        menthods: {
-            updateActiveIndex(index){
-                this.$emit("update:activeIndex", index)
-            }
+export default {
+    props: {
+        contacts: { type: Array, default: [] },
+        activeIndex: { type: Number, default: -1 }
+    },
+    emits: ["update:activeIndex"],
+    methods: {
+        updateActiveIndex(index) {
+            this.$emit("update:activeIndex", index)
         }
     }
+}
 </script>
 
 <template>
     <ul class="list-group">
-        <li class="list-group-item" v-for="(contact, index) in contacts" :key="contact._id" :class="{active: index === activeIndex}">
+        <li class="list-group-item" v-for="(contact, index) in contacts" :key="contact._id"
+            :class="{ active: index === activeIndex }" @click="updateActiveIndex(index)">
             {{ contact.name }}
         </li>
     </ul>
--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -8,7 +8,8 @@
                 Danh bạ
                 <i class="fas fa-address-book"></i>
             </h4>
-            <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts" v-model:activeIndex="activeIndex" />
+            <ContactList v-if="filteredContactsCount > 0" :contacts="filteredContacts"
+                v-model:activeIndex="activeIndex" />
             <p v-else>Không có liên hệ nào.</p>
 
             <div class="mt-3 row justify-content-around align-items-center">
@@ -27,87 +28,94 @@
                     <i class="fas fa-address-card"></i>
                 </h4>
                 <ContactCard :contact="activeContact" />
+                <router-link :to="{
+                name: 'contact.edit',
+                params: { id: activeContact._id },
+            }">
+                    <span class="mt-2 badge badge-warning">
+                        <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+                </router-link>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import ContactCard from '../components/ContactCard.vue';
-    import InputSearch from '../components/InputSearch.vue';
-    import ContactList from '../components/ContactList.vue';
-    import ContactService from "@/services/contact.service";
+import ContactCard from '../components/ContactCard.vue';
+import InputSearch from '../components/InputSearch.vue';
+import ContactList from '../components/ContactList.vue';
+import ContactService from "@/services/contact.service";
 
-    export default {
-        components: {
-            ContactCard, InputSearch, ContactList,
+export default {
+    components: {
+        ContactCard, InputSearch, ContactList,
+    },
+    data() {
+        return {
+            contacts: [],
+            activeIndex: -1,
+            searchText: "",
+        }
+    },
+    watch: {
+        searchText() {
+            this.activeIndex = -1
+        }
+    },
+    computed: {
+        contactStrings() {
+            return this.contacts.map((contact) => {
+                const { name, email, address, phone } = contact;
+                return [name, email, address, phone].join("")
+            })
         },
-        data() {
-            return {
-                contacts:[],
-                activeIndex: -1,
-                searchText: "",
+        filteredContacts() {
+            if (!this.searchText) return this.contacts;
+            return this.contacts.filter((_contact, index) => this.contactString[index].includes(this.searchText))
+        },
+        activeContact() {
+            if (this.activeIndex < 0) return null;
+            return this.filteredContacts[this.activeIndex];
+        },
+        filteredContactsCount() {
+            return this.filteredContacts.length;
+        }
+    },
+    methods: {
+        async retrieveContacts() {
+            try {
+                this.contacts = await ContactService.getAll()
+            } catch (error) {
+                console.log(error);
             }
         },
-        watch: {
-            searchText() {
-                this.activeIndex = -1
-            }
+        refreshList() {
+            this.retrieveContacts();
+            this.activeIndex = -1;
         },
-        computed: {
-            contactStrings(){
-                return this.contacts.map((contact)=>{
-                    const {name,email, address, phone } = contact;
-                    return [name,email,address,phone].join("")
-                })
-            },
-            filteredContacts(){
-                if (!this.searchText) return this.contacts;
-                return this.contacts.filter((_contact,index)=>this.contactString[index].includes(this.searchText))
-            },
-            activeContact(){
-                if (this.activeIndex < 0) return null;
-                return this.filteredContacts[this.activeIndex];
-            },
-            filteredContactsCount() {
-                return this.filteredContacts.length;
-            }
-        },
-        methods: {
-            async retrieveContacts(){
-                try{
-                    this.contacts = await ContactService.getAll()
-                } catch(error){
+        async removeAllContacts() {
+            if (confirm("Bạn muốn xóa tất cả Liên hệ?")) {
+                try {
+                    await ContactService.deleteAll();
+                    this.refreshList();
+                } catch (error) {
                     console.log(error);
                 }
-            },
-            refreshList(){
-                this.retrieveContacts();
-                this.activeIndex = -1;
-            },
-            async removeAllContacts(){
-                if (confirm("Bạn muốn xóa tất cả Liên hệ?")) {
-                    try {
-                        await ContactService.deleteAll();
-                        this.refreshList();
-                    } catch(error){
-                        console.log(error);
-                    }
-                }
-            },
-            goToAddContact(){
-                this.$route.push({name:"contact.add"})
             }
         },
-        mounted(){
-            this.refreshList()
+        goToAddContact() {
+            this.$route.push({ name: "contact.add" })
         }
+    },
+    mounted() {
+        this.refreshList()
     }
+}
 </script>
 
 <style scoped>
-    .page {
-        text-align: left;
-        max-width: 750px;
-    }
+.page {
+    text-align: left;
+    max-width: 750px;
+}
 </style>
